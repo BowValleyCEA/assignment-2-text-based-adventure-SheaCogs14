54a1b481a6053b9099b48a79e55039b9ad71e780
Shea <s.coughlin037@mybvc.ca>
2024-10-29 13:39:06 -0600

    Added classes, added game loop
    Checked game data loads, added switch casing for the commands and one function for now to test.

--- Game.cs
+++ Game.cs
@@ -9,33 +9,68 @@ namespace game1402_a2_starter
 {
     public class GameData
     {
-        public string GameName { get; set; } //This is an example of a property; for whatever reason your serializable data objects will need to be written this way
+        public string GameName { get; set; } 
         public string Description { get; set; }
-        public List<Room> Rooms { get; set; } //this is only an example. You do not ha
-        
-      
+        public List<Room> Rooms { get; set; } = new List<Room>();
 
     }
 
-    public class Game(GameData data)
+    public class Game
     {
-        private GameData _gameData = data;
-
-        public void ProcessString(string enteredString)
+        private GameData _gameData;
+        private Room _currentRoom;
+        public Game(GameData data)
         {
-            enteredString =
-                enteredString.Trim()
-                    .ToLower(); //trim any white space from the beginning or end of string and convert to lower case
-            string[]
-                commands = enteredString
-                    .Split(" "); //split based on spaces. The length of this array will tell you whether you have 1, 2, 3, 4, or more commands.
-            //modify these functions however you want, but this is where the beginning of calling functions to handle where you are
-            string
-                response =
-                    "Default response"; //you will always do something when processing the string and then give a response
-            Console.WriteLine(response); //what you tell the person after what they entered has been processed
+            _gameData = data;
+            _currentRoom = _gameData.Rooms[0];
         }
 
+
+        public void Start()
+        {
+            Console.WriteLine($"Welcome to {_gameData.GameName}");
+            Console.WriteLine(_gameData.Description);
+            UpdateGame();
+
+        }
+
+        private void UpdateGame()
+        {
+            string command;
+            do
+            {
+                Console.WriteLine($"\nYou are in {_currentRoom.Name}. {_currentRoom.Description}");
+                Console.Write("What do you want to do? ");
+                command = Console.ReadLine();
+                commandParse(command);
+            }while (command != "quit");
+        }
+
+        public void commandParse(string command)
+        {
+            command = command.Trim().ToLower();
+            string[] cmds = command.Split(" ");
+
+            switch (cmds.Length)
+            {
+                case 1:
+                    handleSingleCmd(cmds[0]);
+                    break;
+            }
+        }
+
+
+        private void handleSingleCmd(string cmds)
+        {
+            if (cmds == "look")
+            {
+                Console.WriteLine(_currentRoom.Description);
+            }
+            else
+            {
+                Console.WriteLine("Invaild command");
+            }
+        }
     }
 
 }
